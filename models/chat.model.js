6bef36dfeb7b216b9f6a7186d4b825496f19f679
Motaz <mo3taz_hassis@yahoo.com>
2024-04-30 03:46:11 +0300

    Update chat.model.js

--- models/chat.model.js
+++ models/chat.model.js
@@ -14,48 +14,4 @@ const WingSchema = new Schema({
 
 const Wing = mongoose.model('Wing', WingSchema);
 
-const MessageSchema = new Schema({
-    messageContent: {
-        type: String,
-        required: [
-            true,
-            "Title is required"
-        ],
-        minlength: [3, 'Name must be more than 3 charachters!!'],
-    },senderid:{ 
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }}
-    ,{ timestamps: true });
-const Message = mongoose.model('Message', MessageSchema);
-
-
-const UserSchema = new Schema({
-    username: {
-        type: String,
-        required: [
-            true,
-            "Title is required"
-        ],
-        minlength: [3, 'Name must be more than 3 charachters!!']
-    },
-    password: {
-        type: String,
-        required: [
-            true,
-            "Title is required"
-        ],
-        minlength: [3, 'password must be more than 3 charachters!!'],
-    },
-    email: {
-        type: String,
-        required: [
-            true,
-            "Title is required"
-        ],
-        minlength: [3, 'email must be more than 3 charachters!!'],
-    },
-    messages: [MessageSchema]
-}, { timestamps: true });
-const User = mongoose.model('User', UserSchema);
-module.exports ={User,Message,MessageSchema,UserSchema,Wing,WingSchema}
+module.exports ={Wing,WingSchema}
