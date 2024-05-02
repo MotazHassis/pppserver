291b22f1584d0fd2cf98b4c3a704913834e19010
Motaz <mo3taz_hassis@yahoo.com>
2024-05-02 07:10:58 +0300

    Update chat.route.js

--- routes/chat.route.js
+++ routes/chat.route.js
@@ -1,5 +1,7 @@
 const ChatController = require('../controllers/chat.controller');
 module.exports = function(app){
+    app.put('/myupdate/readings', ChatController.updatereading);
+    app.get('/mywing/readings', ChatController.getreadings);
     app.post('/create_wing', ChatController.createWing);
     app.get('/wing/:id', ChatController.findwing);
     app.put('/update/:id', ChatController.updatewing);
