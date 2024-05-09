2abb8825b2d3acbcb853dbb94cb7c1910b48c320
Motaz <mo3taz_hassis@yahoo.com>
2024-05-09 20:37:54 +0300

    Update chat.controller.js

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -7,44 +7,20 @@ const readings={
     
 }
 
-module.exports.updatereading = (req, response) =>{
-    const {right,left} = req.body
+module.exports.updatereadingleft = (req, response) =>{
+    const {left} = req.body
     readings.left=left;
+    return response.json()
+}
+
+module.exports.updatereadingright = (req, response) =>{
+    const {right} = req.body
     readings.right=right;
     return response.json()
 }
 
 module.exports.getreadings = (req, response) =>{
     return response.json(readings)
-}
-
-module.exports.createWing = (request, response) => {
-    const {right,left} = request.body;
-    console.log('reach create method')
-    Wing.create({
-        right,left
-    })
-        .then(thiswing => response.json(thiswing))
-        .catch(err => response.status(400).json(err));
-}
-
-
-module.exports.updatewing = (req, response) =>{
-    const {right,left} = req.body
-    Wing.findOneAndUpdate(
-        { _id: req.params.id },
-        { right: right,left: left },
-        { new: true}
-    )
-    .then(thiswing => response.json(thiswing))
-    .catch(err => response.status(400).json(err));
-    }
-
-
-module.exports.findwing = (req, response) =>{
-    Wing.findOne({_id:req.params.id})
-        .then(thiswing => response.json(thiswing))
-        .catch(err => response.status(400).json(err))
 }
 
 
