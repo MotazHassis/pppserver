b63e7bc8bb417fe17c3fcf86a6c5ef335547ebb7
Motaz <mo3taz_hassis@yahoo.com>
2024-05-10 00:27:13 +0300

    Update chat.controller.js

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -4,7 +4,13 @@ const { response } = require('express');//test
 const readings={
     'right':0,
     'left':0,
+    'speed':0,
+    'rear':0,
     
+}
+
+module.exports.getreadings = (req, response) =>{
+    return response.json(readings)
 }
 
 module.exports.updatereadingleft = (req, response) =>{
@@ -19,9 +25,18 @@ module.exports.updatereadingright = (req, response) =>{
     return response.json()
 }
 
-module.exports.getreadings = (req, response) =>{
-    return response.json(readings)
+module.exports.updatespeed = (req, response) =>{
+    const {speed} = req.body
+    readings.speed=speed;
+    return response.json()
 }
+
+module.exports.updaterear = (req, response) =>{
+    const {rear} = req.body
+    readings.rear=rear;
+    return response.json()
+}
+
 
 
 
